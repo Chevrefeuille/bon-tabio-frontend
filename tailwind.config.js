fb45865a939eb1696110d5dd80342f43b42db526
Adrien Gregorj <adrien.gregorj@gmail.com>
2022-08-15 17:54:29 +0900

    update typeface

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
-/** @type {import('tailwindcss').Config} */
+/** @type {import("tailwindcss").Config} */
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["Poppins"],
+      display: ["Playfair Display"],
+    }
   },
   plugins: [
-    require('@tailwindcss/typography'),
+    require("@tailwindcss/typography"),
   ]
 };
